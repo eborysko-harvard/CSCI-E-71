6027b9776724c88255bb4523e9eb680abdc5c82c
Evan Borysko <eborysko@g.harvard.edu>
2015-09-12 22:45:28 -0400

    added logging to hook

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
- 
+
+export PATH=$PATH:/usr/local/bin
 # save the file as <git_directory>/.git/hooks/pre-commit.d/mvn_test and chmod +x
 LOG_FILE="./pre-commit.log"
 
+rm $LOG_FILE
+
 exec 3>&1 1>>${LOG_FILE} 2>&1
- 
-echo "Running mvn clean test for errors"
+
+echo "---------------------------------------------" | tee -a /dev/fd/3
+echo "Running mvn clean test for errors" | tee -a /dev/fd/3
 
 # retrieving current working directory
 CWD=`pwd`
@@ -17,23 +21,23 @@ MAIN_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $MAIN_DIR/../../csci.e-71.eborysko.hw1/
 NEWCWD=`pwd`
 echo "Working in ${NEWCWD}" | tee -a /dev/fd/3
-echo "Running mvn clean test"
+echo "Running mvn clean test" | tee -a /dev/fd/3
 # running maven clean test
-MVN_RESULT=$( /usr/local/bin/mvn clean test 2>&1)
+MVN_RESULT=$( mvn clean test 2>&1)
 if [ $? -ne 0 ]; then
   echo
   #echo "${MVN_RESULT}" | ((tee /dev/fd/5 | grep -A 10 -B 2 "Reactor Summary:" >/dev/fd/4) 5>&1 | sed -n -e '/^Failed tests:/,/Tests run:.*$/ p' ) 4>&1
-  echo "${MVN_RESULT}"
-  echo
-  echo "Error while testing the code"
+  echo "${MVN_RESULT}" | tee -a /dev/fd/3
+  echo | tee -a /dev/fd/3
+  echo "Error while testing the code" | tee -a /dev/fd/3
   # go back to current working dir
   cd $CWD
   exit 1
 fi
 
-echo "mvn results..."
-echo "$MVN_RESULT"
-echo "Successfully completed mvn clean test"
+echo "mvn results..." | tee -a /dev/fd/3
+echo "$MVN_RESULT" | tee -a /dev/fd/3
+echo "Successfully completed mvn clean test" | tee -a /dev/fd/3
 
 # go back to current working dir
 cd $CWD
